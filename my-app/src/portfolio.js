ad0cb48c97d9ab52fbac538003b3ddbd78ed4e9c
arunkoppoju <jejeeya.koppoju@gmail.com>
2023-10-15 13:42:50 +0530

    first commit

---
+++ my-app/src/portfolio.js
@@ -0,0 +1,118 @@
+import React from "react";
+
+const Portfolio = () => (
+  <div className="portfolio">
+    <div className="header">
+      <h1>Jejeeya Koppoju</h1>
+
+      <h2>Student VIT AP</h2>
+
+      <div className="contact-info">
+        <h3>
+          <a href="jejeeya.koppoju@gmail.com">jejeeya.koppoju@gmail.com</a>
+        </h3>
+
+        <h3>8500403814</h3>
+      </div>
+    </div>
+
+    <div className="about">
+      <h2>About</h2>
+
+      <p className="enlarge">
+        I am VIT AP Student
+        <br />
+        Born on 1st August 2004
+        <br />
+        I have taken CSE (core)
+        <br />
+        My registration number--21bce8280
+      </p>
+    </div>
+
+    <div className="education">
+      <h2>Education</h2>
+
+      <div className="education-item">
+        <h2>DR.KKR GOUTHAM school - Tenali, India</h2>
+
+        <p>10th class 9.7 GPA</p>
+      </div>
+
+      <div className="education-item">
+        <h2>sri Chaitanya college - Tenali , India</h2>
+
+        <p>Inter A Grade</p>
+      </div>
+    </div>
+
+    <div className="at-vit">
+      <h2>AT VIT AP</h2>
+
+      <p className="enlarge">I am a third year student.</p>
+    </div>
+
+    <div className="skills">
+      <h2>Skills</h2>
+
+      <div className="talent">
+        <h2>Java</h2>
+
+        <p>Core and Advanced java specialist</p>
+      </div>
+
+      <div className="talent">
+        <h2>JavaScript</h2>
+
+        <p>
+          Can create complex web application front end scripting using JS and
+          jQuery
+        </p>
+      </div>
+
+      <div className="talent">
+        <h2>HTML</h2>
+
+        <p>Strong in writing HTML for UI design</p>
+
+        <br></br>
+
+        <br></br>
+      </div>
+    </div>
+
+    <div className="interests">
+      <h2>Interests and Hobbies</h2>
+
+      <ul className="talent">
+        <li>Reading Books</li>
+
+        <li>Playing Cricket</li>
+      </ul>
+
+      <ul className="talent">
+        <li>Coding</li>
+
+        <li>Watching Movies</li>
+      </ul>
+    </div>
+
+    <br></br>
+
+    <br></br>
+
+    <br></br>
+
+    <br></br>
+
+    <div className="footer">
+      <p>
+        jejeeya koppoju&mdash;{" "}
+        <a href="jejeeya.koppoju@gmail.com">jejeeya.koppoju@gmail.com</a>{" "}
+        &mdash; 8500403814
+      </p>
+    </div>
+  </div>
+);
+
+export default Portfolio;
